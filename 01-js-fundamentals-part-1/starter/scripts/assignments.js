1c33a375285af79d3005649c20289f0b23929386
resident7709 <resident7709@gmail.com>
2023-04-19 15:08:00 +0300

    assignments.js been changed

---
+++ 01-js-fundamentals-part-1/starter/scripts/assignments.js
@@ -0,0 +1,18 @@
+let country = "Russia";
+const continent = "Eurasia";
+let population = 143.4;
+
+let language;
+language = "Russian";
+country = "MotherRussia";
+
+console.log(country, continent, population);
+
+const isIsland = false;
+
+console.log(
+  typeof isIsland,
+  typeof population,
+  typeof country,
+  typeof language
+);
