1aabbd03485a9b75130d94ee9121939596b59a78
resident7709 <resident7709@gmail.com>
2023-04-19 17:08:07 +0300

    before challenge#2 commit

--- 01-js-fundamentals-part-1/starter/scripts/challenges.js
+++ 01-js-fundamentals-part-1/starter/scripts/challenges.js
@@ -1,5 +1,5 @@
-// *CHALLENGE#1
-// *Data1
+// * CHALLENGE#1
+// * Data1
 let weightMark = 78;
 let heightMark = 1.69;
 let weightJohn = 92;
@@ -13,7 +13,7 @@ console.log("and John's BMI: " + bmiJohn.toFixed(2));
 let markHigherBMI = bmiMark > bmiJohn;
 console.log("markHigherBMI = " + markHigherBMI);
 
-// *Data2
+// * Data2
 weightMark = 95;
 heightMark = 1.88;
 weightJohn = 85;
@@ -27,4 +27,12 @@ console.log("and John's BMI: " + bmiJohn.toFixed(2));
 markHigherBMI = bmiMark > bmiJohn;
 console.log("markHigherBMI = " + markHigherBMI);
 
-// *CHALLENGE#2
+// * CHALLENGE#2
+// * Use the BMI example from Challenge #1, and the code you already wrote, and
+// * improve it.
+// * Your tasks:
+// * 1. Print a nice output to the console, saying who has the higher BMI. The message
+// * is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+// * 2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+// * BMI (28.3) is higher than John's (23.9)!"
+// * Hint: Use an if/else statement
