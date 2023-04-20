214e98e36e04797ced5924a502d50c83a135f1fa
resident7709 <resident7709@gmail.com>
2023-04-20 18:26:01 +0300

    'logical operators assignment' commit

--- 01-js-fundamentals-part-1/starter/scripts/assignments.js
+++ 01-js-fundamentals-part-1/starter/scripts/assignments.js
@@ -1,41 +1,65 @@
-let myCountry;
-const myCountryContinent = "Eurasia";
-let myCountryPopulation = 143.4;
+let country = "Russia";
 
-let halfPopulation = myCountryPopulation / 2;
+let language = "russian";
+let population = 143.4;
+let isIsland = false;
 
-myCountryPopulation = ++myCountryPopulation;
+const countryForSarah = language == "english" && population < 50 && !isIsland;
 
-let myCountryLanguage;
-myCountryLanguage = "russian";
-myCountry = "Russia";
+if (countryForSarah) {
+  console.log(`You should live in ${country}!`);
+} else console.log(`${country} does not meet your criteria :(`);
 
-const isIsland = false;
+// const numNeighbours = Number(
+//   prompt("How many neighbour countries does your country have?")
+// );
 
-let finlandPopulation = 6;
-let averagePopulation = 33;
+// if (numNeighbours === 1) {
+//   console.log("Only 1 border!");
+// } else if (numNeighbours >= 1) {
+//   console.log("More than 1 border..");
+// } else console.log("No borders..");
 
-let description =
-  myCountry +
-  " is in " +
-  myCountryContinent +
-  ", and its " +
-  myCountryPopulation +
-  " million people speak " +
-  myCountryLanguage +
-  ".";
+// console.log(numNeighbours);
 
-let description1 = `${myCountry} is in ${myCountryContinent}, and its ${myCountryPopulation} million people speak ${myCountryLanguage}.`;
+// let myCountry;
+// const myCountryContinent = "Eurasia";
+// let myCountryPopulation = 143.4;
 
-if (myCountryPopulation > averagePopulation) {
-  console.log(`${myCountry}'s population is above average.`);
-} else {
-  console.log(
-    `${myCountry}'s population is ${
-      averagePopulation - myCountryPopulation
-    } million below average.`
-  );
-}
+// let halfPopulation = myCountryPopulation / 2;
+
+// myCountryPopulation = ++myCountryPopulation;
+
+// let myCountryLanguage;
+// myCountryLanguage = "russian";
+// myCountry = "Russia";
+
+// const isIsland = false;
+
+// let finlandPopulation = 6;
+// let averagePopulation = 33;
+
+// let description =
+//   myCountry +
+//   " is in " +
+//   myCountryContinent +
+//   ", and its " +
+//   myCountryPopulation +
+//   " million people speak " +
+//   myCountryLanguage +
+//   ".";
+
+// let description1 = `${myCountry} is in ${myCountryContinent}, and its ${myCountryPopulation} million people speak ${myCountryLanguage}.`;
+
+// if (myCountryPopulation > averagePopulation) {
+//   console.log(`${myCountry}'s population is above average.`);
+// } else {
+//   console.log(
+//     `${myCountry}'s population is ${
+//       averagePopulation - myCountryPopulation
+//     } million below average.`
+//   );
+// }
 
 // console.log(description);
 // console.log(description1);
--- 01-js-fundamentals-part-1/starter/scripts/script.js
+++ 01-js-fundamentals-part-1/starter/scripts/script.js
@@ -1,20 +1,110 @@
-const age = 13;
+// * logical operators
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+const isTired = true; // C
 
-if (age >= 18) {
-  console.log("You can drive!!🚗");
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense); // ! = NOT operator
+console.log(!hasGoodVision); // ! = NOT operator
+
+const shouldDrive = hasDriversLicense && hasGoodVision && !isTired;
+
+if (shouldDrive) {
+  console.log("able to drive");
 } else {
-  const yearsLeft = 18 - age;
-  console.log(`You can start drive after ${yearsLeft} years..`);
+  console.log("not able to drive yet");
 }
 
-const birthYear = 2003;
+// * equality operators: ==&===
+// const age = "18";
 
-let century;
+// if (age === 18) console.log("Cool!! (strict)");
 
-if (birthYear <= 2000) {
-  century = 20;
-} else {
-  century = 21;
-}
+// if (age == 18) console.log("Cool!! (loose)");
 
-console.log(century);
+// const someNumber = Number(prompt("What's your number?"));
+
+// console.log(someNumber);
+
+// if (someNumber === 23) {
+//   console.log("Cool number..");
+// } else if (someNumber === 7) {
+//   console.log("7 is also coll number..");
+// } else {
+//   console.log("num isn't 23 or 7");
+// }
+
+// if (someNumber !== 23) console.log("Oops..");
+
+// * 5 falsy values: 0, "", undefined, null, NaN
+// console.log(Boolean(0));
+// console.log(Boolean("Hi.."));
+// console.log(Boolean(undefined));
+// console.log(Boolean(null));
+// console.log(Boolean({}));
+
+// const myMoney = Infinity;
+
+// if (myMoney) {
+//   console.log("Thanks GOD!!💥");
+// } else {
+//   console.log("You should get a job!!");
+// }
+
+// let height;
+// height = "2";
+
+// if (height) {
+//   console.log("yes");
+// } else {
+//   console.log("no");
+// }
+
+// * type conversion
+// const inputYear = "1991";
+
+// console.log(Number(inputYear), inputYear);
+// console.log(+inputYear + 18);
+
+// console.log(Number("Alex"));
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+// * type coercion
+// console.log("I'm " + 23 + " years old..");
+// console.log("I'm " + "23" + " years old..");
+
+// console.log("23" - 2 - "5");
+
+// todo: assignment
+// let someVar = "9" - "5"; // 4
+// someVar = "19" - "13" + "17"; // "617"
+// someVar = "19" - "13" + 17; // 23
+// someVar = "123" < 57; // false
+// someVar = 5 + 6 + "4" + 9 - 4 - 2; // 1143
+
+// console.log(someVar);
+
+// * if/else
+// const age = 13;
+
+// if (age >= 18) {
+//   console.log("You can drive!!🚗");
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(`You can start drive after ${yearsLeft} years..`);
+// }
+
+// const birthYear = 2003;
+
+// let century;
+
+// if (birthYear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
+
+// console.log(century);
