87721a815531c70e3118e5e01ad4f5ff5dae8972
resident7709 <resident7709@gmail.com>
2023-04-22 15:03:46 +0300

    functions assignment done..

---
+++ 02-js-fundamentals-part-2/starter/src/assignments.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// * Functions
+// function describeCountry(country, population, capitalCity) {
+//   return `${country} has ${population} million people and its
+//     capital city is ${capitalCity}`;
+// }
+
+// const descFinland = describeCountry("Finland", 6, "Helsinki");
+// const descRussia = describeCountry("Russia", 143, "Moscow");
+// const descChina = describeCountry("China", 1412, "Beijing");
+
+// console.log(descFinland);
+// console.log(descRussia);
+// console.log(descChina);
+
+// * Function Declarations vs. Expressions
---
+++ 02-js-fundamentals-part-2/starter/src/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// * Function Declarations vs. Expressions
+
+// * Functions
+// function logger() {
+//   console.log("My name is Alex!!");
+// }
+
+// logger(); // вызов функции
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges for you!!😍`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(3, "no");
+// const orangeJuice = fruitProcessor("no", 3);
+
+// console.log(appleJuice);
+// console.log(orangeJuice);
