b7b22893402da1a3035846a52fd6bdbccead7203
resident7709 <resident7709@gmail.com>
2023-04-26 19:09:22 +0300

    ..coding challenge#1

--- 03-developer-skills/starter/script.js
+++ 03-developer-skills/starter/script.js
@@ -1,32 +1,77 @@
 // * Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
-const temperatures = [3, -2, -6, -1, 3, "error", 9, 13, 17, 15, 14, 9, 5];
+// const temperatures = [3, -2, -6, -1, 3, "error", 9, 13, 17, 15, 14, 9, 5];
 
-const amplitutde = calcTempAmplitude(temperatures);
+// const amplitutde = calcTempAmplitude(temperatures);
 
-function calcTempAmplitude(temps) {
-  let max = temps[0];
-  let min = temps[0];
+// function calcTempAmplitude(temps) {
+//   let max = temps[0];
+//   let min = temps[0];
 
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== "number") continue;
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== "number") continue;
 
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
-  }
-  console.log(min, max);
-  return max - min;
-}
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//     console.log(min, max);
+//   return max - min;
+// }
 
-console.log(amplitutde);
+// console.log(amplitutde);
 
 // * merging two arrays
-const amplitutdeNew = calcTempAmplitudeNew([2, 4, 6], [8, 9, 5]);
 
-function calcTempAmplitudeNew(temps1, temps2) {
+// const amplitutdeNew = calcTempAmplitudeNew([2, 4, 6], [8, 9, 5]);
+
+// function calcTempAmplitudeNew(temps1, temps2) {
+//   const temps = temps1.concat(temps2);
+
+//   let max = temps[0];
+//   let min = temps[0];
+
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== "number") continue;
+
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//     console.log(min, max);
+//   return max - min;
+// }
+
+// console.log(amplitutdeNew);
+
+function measureKelvin() {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    // value: +prompt("Degrees celcius:"),
+    value: 10,
+  };
+
+  console.table(measurement);
+
+  //   console.log(measurement.value);
+  //   console.warn(measurement.value);
+  //   console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+}
+
+console.log(measureKelvin());
+
+// * using a debugger
+
+function calcTempAmplitudeBug(temps1, temps2) {
   const temps = temps1.concat(temps2);
+
+  //   let max = 0; // ! bugged
+  //   let min = 0; // ! bugged
 
   let max = temps[0];
   let min = temps[0];
@@ -42,4 +87,6 @@ function calcTempAmplitudeNew(temps1, temps2) {
   return max - min;
 }
 
-console.log(amplitutdeNew);
+const amplitutdeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// ! identify a bug
+console.log(amplitutdeBug);
