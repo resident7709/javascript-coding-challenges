62d58dfcfe05abbdfb1c45543c501f7b3b9fb14f
resident7709 <resident7709@gmail.com>
2023-04-26 17:00:23 +0300

    ..debugging (fixing errors)

--- 03-developer-skills/starter/script.js
+++ 03-developer-skills/starter/script.js
@@ -1,2 +1,45 @@
 // * Remember, we're gonna use strict mode in all scripts now!
 "use strict";
+
+const temperatures = [3, -2, -6, -1, 3, "error", 9, 13, 17, 15, 14, 9, 5];
+
+const amplitutde = calcTempAmplitude(temperatures);
+
+function calcTempAmplitude(temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(min, max);
+  return max - min;
+}
+
+console.log(amplitutde);
+
+// * merging two arrays
+const amplitutdeNew = calcTempAmplitudeNew([2, 4, 6], [8, 9, 5]);
+
+function calcTempAmplitudeNew(temps1, temps2) {
+  const temps = temps1.concat(temps2);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(min, max);
+  return max - min;
+}
+
+console.log(amplitutdeNew);
