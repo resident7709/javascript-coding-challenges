edb03773e71c8fe5548681e978a369e622144768
resident7709 <resident7709@gmail.com>
2023-04-27 18:14:57 +0300

    ..manipulating css styles

--- 05-guess-my-number/starter/script.js
+++ 05-guess-my-number/starter/script.js
@@ -1,1 +1,40 @@
-'use strict';
+"use strict";
+
+// * state vars
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector(".number").textContent = secretNumber;
+
+document.querySelector(".check").addEventListener("click", () => {
+  const guess = +document.querySelector(".guess").value;
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔ No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number!";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📉 Too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
+
+// document.querySelector(".number").textContent = 13;
+
+// console.log(document.querySelector(".guess").value);
