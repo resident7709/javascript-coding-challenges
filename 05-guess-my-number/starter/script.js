79a9bf15bbd9baa179b0aefd562474d5e774eca6
resident7709 <resident7709@gmail.com>
2023-04-27 18:35:14 +0300

    ..coding challenge#1

--- 05-guess-my-number/starter/script.js
+++ 05-guess-my-number/starter/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// * state vars
+// * State Vars
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
@@ -8,13 +8,19 @@ document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", () => {
   const guess = +document.querySelector(".guess").value;
-  console.log(guess, typeof guess);
-
+  // ! no input
   if (!guess) {
     document.querySelector(".message").textContent = "⛔ No number!";
-  } else if (guess === secretNumber) {
+  }
+  // ! player wins
+  else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
-  } else if (guess > secretNumber) {
+    // todo: styles changing (..always in strings!!)
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+  }
+  // ! too high
+  else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📈 Too high!";
       score--;
@@ -23,7 +29,9 @@ document.querySelector(".check").addEventListener("click", () => {
       document.querySelector(".message").textContent = "💥 You lost the game!";
       document.querySelector(".score").textContent = 0;
     }
-  } else if (guess < secretNumber) {
+  }
+  // ! too low
+  else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📉 Too low!";
       score--;
@@ -34,7 +42,3 @@ document.querySelector(".check").addEventListener("click", () => {
     }
   }
 });
-
-// document.querySelector(".number").textContent = 13;
-
-// console.log(document.querySelector(".guess").value);
