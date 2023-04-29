1e6cac492c2206db4f826d8c5dab616f9a6539df
resident7709 <resident7709@gmail.com>
2023-04-29 14:35:45 +0300

    ..scope and scope chain

--- 08-behind-the-scenes/starter/script.js
+++ 08-behind-the-scenes/starter/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+// * Call Stack: Global execution, first() called, second() called, second() returns, first() returns, back to Global..
+
+const nameFirst = "Alex"; // * Global Execution => Call Stack
+
+const first = () => {
+  let a = 1;
+  const b = second(7, 9);
+  a += b;
+  return a;
+};
+
+function second(x, y) {
+  var c = 2;
+  return c;
+}
+
+const x = first();
+
+console.log(x);
