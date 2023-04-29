b3422b4b7a47633bda8d9adc5d48d6a172044f28
resident7709 <resident7709@gmail.com>
2023-04-29 09:58:03 +0300

    ..resetting the game

--- 07-pig-game/starter/script.js
+++ 07-pig-game/starter/script.js
@@ -24,29 +24,61 @@ diceEl.classList.add("hidden");
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = () => {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
 
 // * Rolling dice functionality
 btnRoll.addEventListener("click", () => {
-  // todo: generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
+  if (playing) {
+    // todo: generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  // todo: display dice
-  diceEl.classList.remove("hidden");
-  diceEl.src = `dice-${dice}.png`;
+    // todo: display dice
+    diceEl.classList.remove("hidden");
+    diceEl.src = `dice-${dice}.png`;
 
-  // todo: check for rolled 1, if true, switch to the next player
-  if (dice !== 1) {
-    // * add dice to current score
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // * switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle("player--active");
-    player1El.classList.toggle("player--active");
+    // todo: check for rolled 1, if true, switch to the next player
+    if (dice !== 1) {
+      // * add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // * switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener("click", () => {
+  if (playing) {
+    // todo: add current score to active's player score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // todo: check if player's score >= 100
+    if (scores[activePlayer] >= 100) {
+      // todo: finish the game
+      playing = false;
+      diceEl.classList.add("hidden");
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      // todo: switch to the next player
+      switchPlayer();
+    }
   }
 });
