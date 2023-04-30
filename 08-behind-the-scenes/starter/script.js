731a87f792682f6865260b7820befe2b651e9699
resident7709 <resident7709@gmail.com>
2023-04-30 17:01:52 +0300

    ..this keyword

--- 08-behind-the-scenes/starter/script.js
+++ 08-behind-the-scenes/starter/script.js
@@ -1,39 +1,93 @@
 "use strict";
 
+// * this keyword
+
+// * Hoisting and TDZ
+// * Vars
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+// console.log(x === window.x);
+// console.log(y === window.y);
+// console.log(z === window.z);
+
+// console.log(firstName);
+// console.log(job);
+// console.log(year);
+
+// var firstName = "Jonas";
+// let job = "teacher";
+// const year = 1991;
+
+// * Functions
+// console.log(numProducts);
+// if (!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//   console.log("All products deleted!");
+// }
+
+// console.log(addDecl(2, 1));
+// console.log(addExpr(2, 1));
+// console.log(addArrow(2, 1));
+
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// const addArrow = (a, b) => a + b;
+
+// const myName = "Jonas";
+
+// if (myName === "Jonas") {
+//   console.log(`Jonas is a ${job}`);
+//   const age = 2037 - birtYear;
+//   console.log(age);
+//   const job = "teacher"; // ! const job in TDZ
+//   console.log(x);
+// }
+
 // * Scope Chain vs Call Stack
-function calcAge(birthYear) {
-  const age = 2023 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2023 - birthYear;
 
-  function printAge() {
-    let output = `${firstName} ${age} years old, and born in ${birthYear}..`;
-    console.log(output);
+//   function printAge() {
+//     let output = `${firstName} ${age} years old, and born in ${birthYear}..`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
-      const firstName = " Steven"; // ! new var
-      output = "NEW OUTPUT!!"; // ! reassigned var
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       const firstName = " Steven"; // ! new var
+//       output = "NEW OUTPUT!!"; // ! reassigned var
 
-      const str = `..you're a millenial, ${firstName}!`;
-      console.log(str);
+//       const str = `..you're a millenial, ${firstName}!`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
+//       function add(a, b) {
+//         return a + b;
+//       }
 
-      console.log(firstName);
-      console.log(output);
-    }
-    console.log(millenial);
-    // console.log(add(2, 3)); // * under strict mode
-  }
-  printAge();
+//       console.log(firstName);
+//       console.log(output);
+//     }
+//     console.log(millenial);
+// console.log(add(2, 3)); // * under strict mode
+//   }
+//   printAge();
 
-  return age;
-}
+//   return age;
+// }
 
-const firstName = "Jonas";
+// const firstName = "Jonas";
 
-calcAge(1991);
+// calcAge(1991);
 
 // const myName = "Alex Romanov";
 
