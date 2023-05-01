30b8a2a61425f2d76c44e92566cb2205423a8868
resident7709 <resident7709@gmail.com>
2023-05-01 11:57:38 +0300

    ..primitives vs objects in practice

--- 08-behind-the-scenes/starter/script.js
+++ 08-behind-the-scenes/starter/script.js
@@ -1,5 +1,26 @@
 "use strict";
 
+// * Primitives vs Objects
+
+// let age = 30;
+// let oldAge = age;
+// age = 31;
+
+// console.log(age);
+// console.log(oldAge);
+
+// const jonas = {
+//   name: "Jonas",
+//   age: 30,
+// };
+
+// const kai = jonas;
+// kai.name = "Kai";
+// kai.age = 37;
+
+// console.log("Jonas:", jonas);
+// console.log("Kai:", kai);
+
 // * Regular Functions vs Arrow Functions
 // const jonas = {
 //   name: "Jonas",
