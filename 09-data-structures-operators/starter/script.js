dd03c0f5f8f9fd04a1a538521cffd471c2d84fe3
resident7709 <resident7709@gmail.com>
2023-05-02 15:24:17 +0300

    ..spread operator (...)

--- 09-data-structures-operators/starter/script.js
+++ 09-data-structures-operators/starter/script.js
@@ -12,25 +12,79 @@ const restaurant = {
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
 
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
   order: function (starterInd, mainInd) {
     return [this.starterMenu[starterInd], this.mainMenu[mainInd]];
   },
 
-  // openingHours: {
-  //   thu: {
-  //     open: 12,
-  //     close: 22,
-  //   },
-  //   fri: {
-  //     open: 11,
-  //     close: 23,
-  //   },
-  //   sat: {
-  //     open: 0, // Open 24 hours
-  //     close: 24,
-  //   },
-  // },
+  orderDelivery: function ({
+    starterInd = 1,
+    mainInd = 0,
+    time = "20:00",
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterInd]} and ${this.mainMenu[mainInd]} will be delivered to ${address}
+      at ${time}`
+    );
+  },
 };
+
+// * Destructuring Objects
+
+restaurant.orderDelivery({
+  time: "22:30",
+  address: "Via del Sole, 21",
+  mainInd: 2,
+  starterInd: 2,
+});
+restaurant.orderDelivery({
+  // time: "22:30", // * default value
+  address: "Via del Sole, 21",
+  // mainInd: 2, // * default value
+  starterInd: 3,
+});
+
+const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+// console.log(restaurantName, hours, tags);
+
+// * default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
+
+// * mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 9, b: 8, c: 14 };
+({ a, b } = obj);
+// console.log(a, b);
+
+// * nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+// console.log(o, c);
 
 // * Destructuring Arrays
 
