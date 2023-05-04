aa488da856e5f6e8d85e462df5b7b73adf857cab
resident7709 <resident7709@gmail.com>
2023-05-04 13:38:43 +0300

    ..maps iteration

--- 09-data-structures-operators/starter/script.js
+++ 09-data-structures-operators/starter/script.js
@@ -53,7 +53,38 @@ const restaurant = {
   },
 };
 
+// * MAPS Iteration
+
 // * MAPS
+const rest = new Map();
+rest.set("name", "Classico Italiano");
+rest.set(1, "Firenze, Italy");
+rest.set(2, "Lisbon, Portugal");
+
+rest
+  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+  .set("open", 11)
+  .set("close", 23)
+  .set(true, "We are open")
+  .set(false, "We are closed");
+
+rest.delete(2);
+// rest.clear();
+const arr = [1, 2]; // ! because reference type..
+rest.set(arr, "Test");
+rest.set(document.querySelector("h1"), "Heading");
+console.log(rest.get(arr));
+
+// const time = 21;
+// console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+
+console.log(rest);
+// console.log(rest.get("name"));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
+
+// console.log(rest.has("categories"));
+// console.log(rest.size);
 
 // * SETS
 // const ordersSet = new Set([
