72639c3a126be0629bace128699059ef4e043afe
resident7709 <resident7709@gmail.com>
2023-05-04 11:30:03 +0300

    ..sets

--- 09-data-structures-operators/starter/challenges.js
+++ 09-data-structures-operators/starter/challenges.js
@@ -42,6 +42,53 @@ const game = {
 };
 
 // * CHALLENGE#2
+// todo: 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+// console.log("1. ==============================");
+// * just for loop..
+// for (let i = 0; i < game.scored.length; i++)
+//   console.log(`Goal ${i + 1}: ${game.scored[i]}`);
+// * for of..destructuring..
+for (const [i, player] of game.scored.entries())
+  console.log(`Goal ${i + 1}: ${player}`);
+
+// todo: 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+// console.log("2. ==============================");
+const values = Object.values(game.odds);
+
+// * Arrow function
+// const getAverage = num => {
+//   let sum = 0;
+//   for (let i = 0; i < values.length; i++) {
+//     sum += num[i] / num.length;
+//   }
+//   return +sum.toFixed(2);
+// };
+
+// console.log(getAverage(values));
+
+// * for of..
+let sum = 0;
+for (const odd of values) sum += odd / values.length;
+
+// console.log(+sum.toFixed(2));
+
+// todo: 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this: Odd of victory Bayern Munich: 1.33 Odd of draw: 3.25 Odd of victory Borrussia Dortmund: 6.5 Get the team names directly from the game object, don't hardcode them (except for "draw"). Hint: Note how the odds and the game objects have the same property names
+// console.log("3. ==============================");
+// * for of..destructuring..ternary operator..
+// for (const [team, odd] of Object.entries(game.odds)) {
+//   const teamStr = team === "x" ? "draw" : `${game[team]}`;
+
+//   console.log(`Odd of victory ${teamStr}: ${odd}`);
+// }
+
+// todo: 4. Bonus: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this: { Gnarby: 1, Hummels: 1, Lewandowski: 2 }
+// console.log("BONUS. ==========================");
+const scorers = {};
+
+for (const player of game.scored)
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+
+// console.log(scorers);
 
 // * CHALLENGE#1
 // todo: 1.Create one player array for each team (variables 'players1' and 'players2')..
@@ -69,14 +116,14 @@ let {
 // todo: 6. Write a function ('printGoals') that receives an arbitrary number of player names (not an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
 
 // todo: Test data for 6 = First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.Then, call the function again with players from game.scored
-function printGoals(...players) {
-  console.log(`${players.length} goals were scored by ${players}`);
-}
+// function printGoals(...players) {
+//   console.log(`${players.length} goals were scored by ${players}`);
+// }
 
 // printGoals("Davies", "Muller", "Lewandowski", "Kimmich");
 // printGoals("Davies", "Muller");
 // printGoals(...game.scored); // ! Just Spread Operator !
 
 // todo: 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, without using an if/else statement or the ternary operator.
-team1 < team2 && console.log("Bayern Munich is more likely to win!!");
-team1 > draw && console.log("Borussia Dortmund is more likely to win!!");
+// team1 < team2 && console.log("Bayern Munich is more likely to win!!");
+// team1 > draw && console.log("Borussia Dortmund is more likely to win!!");
