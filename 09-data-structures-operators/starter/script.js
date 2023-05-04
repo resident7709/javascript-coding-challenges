7b6bb9848a139cf5a482d9aa703b45a79f19be67
resident7709 <resident7709@gmail.com>
2023-05-04 07:43:29 +0300

    ..coding challenge #2

--- 09-data-structures-operators/starter/script.js
+++ 09-data-structures-operators/starter/script.js
@@ -53,6 +53,29 @@ const restaurant = {
   },
 };
 
+// * Looping Objects
+
+// * Property NAMES
+// const properties = Object.keys(openingHours);
+// console.log(properties);
+// let openStr = `We are open on ${properties.length} days: `;
+// for (const day of properties) {
+//   openStr += `${day}, `;
+// }
+// console.log(openStr);
+
+// * Property VALUES
+// const values = Object.values(openingHours);
+// console.log(values);
+
+// * Entire Object
+// const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// for (const [day, { open, close }] of entries) {
+//   console.log(`On ${day} we are open at ${open} and close at ${close}`);
+// }
+
 // * Optional Chaining Operator
 // if (restaurant.openingHours && restaurant.openingHours.mon)
 //   console.log(restaurant.openingHours.mon.open);
