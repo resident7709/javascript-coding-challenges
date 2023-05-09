872249df07ca65cdd362f5cf3fd4426262c534a7
resident7709 <resident7709@gmail.com>
2023-05-09 19:48:14 +0300

    ..arrays-coding-challenge#1..

---
+++ 11-arrays-bankist/starter/challenges.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// *CHALLENGE#1
+const dogsJulia = [3, 5, 2, 12, 7];
+const dogsKate = [4, 1, 15, 8, 3];
+
+const checkDogs = function (a, b) {
+  console.log(a);
+  console.log(b);
+};
+
+checkDogs(dogsJulia, dogsKate);
