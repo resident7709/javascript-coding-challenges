18badc4a1cab8e557ea179a881fe92810545b12f
resident7709 <resident7709@gmail.com>
2023-05-09 11:16:28 +0300

    ..looping arrays foEach

--- 11-arrays-bankist/starter/script-lectures.js
+++ 11-arrays-bankist/starter/script-lectures.js
@@ -1,12 +1,23 @@
 "use strict";
 
-// * 004.THE NEW AT METHOD
+// * LOOPING ARRAYS .forEach
+
+// * 004.THE NEW .at METHOD
+// const arr = [23, 11, 64];
+
+// * getting last array element
+// console.log(arr[arr.length - 1]);
+// console.log(arr.slice(-1)[0]);
+// console.log(arr.at(-1));
+
+// console.log("String".at(0));
+// console.log("String".at(-1));
 
 // * 003.SIMPLE ARRAY METHODS
-let arr = ["a", "b", "c", "d", "e"];
-const arr2 = ["f", "g", "h", "i", "j"];
+// let arr = ["a", "b", "c", "d", "e"];
+// const arr2 = ["f", "g", "h", "i", "j"];
 
-// * Slice - не изменяет оригинал
+// * .slice - не изменяет оригинал
 // console.log(arr.slice()); // * shallow copy..
 // console.log([...arr]); // * ES6
 // console.log(arr.slice(2, 4));
@@ -14,23 +25,23 @@ const arr2 = ["f", "g", "h", "i", "j"];
 // console.log(arr.slice(-1)); // * (-1) always last element..
 // console.log(arr.slice(1, -2));
 
-// * Splice - изменяет оригинал
+// * .splice - изменяет оригинал
 // console.log(arr.splice(2));
 // arr.splice(-1);
 // console.log(arr);
 // arr.splice(1, 2); // * (start, deleteCount)
 // console.log(arr);
 
-// * Reverse - изменяет оригинал
+// * .reverse - изменяет оригинал
 // console.log(arr2.reverse());
 // console.log(arr2);
 
-// * Concat - не изменяет оригиналы, но возвращает новый массив
+// * .concat - не изменяет оригиналы, но возвращает новый массив
 // const letters = arr.concat(arr2);
 // const letters2 = [...arr, ...arr2]; // * ES6
 
 // console.log(letters);
 // console.log(letters2);
 
-// * Join - возвращает новую строку, объединяя все элементы массива
+// * .join - возвращает новую строку, объединяя все элементы массива
 // console.log(letters.join(" - "));
