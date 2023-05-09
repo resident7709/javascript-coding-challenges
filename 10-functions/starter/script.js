58d308f48b63951ad9c81612df215a4bbde4f8da
resident7709 <resident7709@gmail.com>
2023-05-09 08:43:23 +0300

    ..coding challenge #2

--- 10-functions/starter/script.js
+++ 10-functions/starter/script.js
@@ -1,22 +1,63 @@
 "use strict";
 
 // * CLOSURES
-const secureBooking = function () {
-  let passengerCount = 0;
+// const secureBooking = function () {
+//   let passengerCount = 0;
 
-  return function () {
-    passengerCount++;
-    console.log(`${passengerCount} passengers`);
-  };
-};
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
 
-const booker = secureBooking();
+// const booker = secureBooking();
 
-booker();
-booker();
-booker();
+// booker();
+// booker();
+// booker();
 
-console.dir(booker);
+// console.dir(booker);
+
+// * More Closure Examples
+// * Example 1
+// let f;
+
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
+
+// const h = function () {
+//   const b = 777;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
+
+// g();
+// f();
+// console.dir(f);
+// * f reassigned by h..
+// h();
+// f();
+// console.dir(f);
+
+// * Example 2
+// const boardPassengers = function (num, wait) {
+//   const perGroup = num / 3;
+
+//   setTimeout(() => {
+//     console.log(`We're boarding all ${num} passengers`);
+//     console.log(`There are 3 groups, each with ${perGroup} passengers`);
+//   }, wait * 1000);
+
+//   console.log(`Will start boarding in ${wait} seconds`);
+// };
+
+// const perGroup = 1000;
+// boardPassengers(180, 3);
 
 // * IIFE
 // const runOnce = function () {
