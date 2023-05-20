533cdb74082afee5654a2c331da6eaeca7ac03c5
resident7709 <resident7709@gmail.com>
2023-05-20 08:47:44 +0300

    ..selecting-creating-and-deleting-elements

--- 13-advanced-dom-and-events/starter/script.js
+++ 13-advanced-dom-and-events/starter/script.js
@@ -6,14 +6,14 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 
-const openModal = function (e) {
+const openModal = e => {
   e.preventDefault();
 
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
 
-const closeModal = function () {
+const closeModal = () => {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
