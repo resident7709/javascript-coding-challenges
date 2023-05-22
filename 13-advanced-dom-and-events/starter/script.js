479b69a4cebd976d44ae5470daeb6c1434c6dba7
resident7709 <resident7709@gmail.com>
2023-05-22 18:03:44 +0300

    ..revealing-elements-on-scroll

--- 13-advanced-dom-and-events/starter/script.js
+++ 13-advanced-dom-and-events/starter/script.js
@@ -92,19 +92,49 @@ const handleHover = function (e) {
 nav.addEventListener("mouseover", handleHover.bind(0.5));
 nav.addEventListener("mouseout", handleHover.bind(1));
 
-// * Sticky Navigation
-const initialCoords = section1.getBoundingClientRect();
+// * Sticky Navigation: Intersection Observer API
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
 
-console.log(initialCoords);
-
-window.addEventListener("scroll", () => {
-  console.log(window.scrollY);
-
-  if (window.scrollY > initialCoords.top) nav.classList.add("sticky");
+const stickyNav = entries => {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) nav.classList.add("sticky");
   else nav.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
+headerObserver.observe(header);
 
 // * ============================================== * //
+// * Sticky Navigation
+// const initialCoords = section1.getBoundingClientRect();
+
+// console.log(initialCoords);
+
+// window.addEventListener("scroll", () => {
+//   console.log(window.scrollY);
+
+//   if (window.scrollY > initialCoords.top) nav.classList.add("sticky");
+//   else nav.classList.remove("sticky");
+// });
+
+// const obsCallback = (entries, observer) => {
+//   entries.forEach(entry => console.log(entry));
+// };
+
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+
 // * Tabbed Component
 // tabs.forEach(tb => tb.addEventListener("click", () => console.log("tab")));
 
