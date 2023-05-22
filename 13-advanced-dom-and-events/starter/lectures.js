fd5d0459d4e338c9edf6de1de3c1227d0243bb4e
resident7709 <resident7709@gmail.com>
2023-05-22 08:48:35 +0300

    ..building-a-tabbed-component

--- 13-advanced-dom-and-events/starter/lectures.js
+++ 13-advanced-dom-and-events/starter/lectures.js
@@ -1,6 +1,38 @@
 "use strict";
 
 // * 012.DOM Traversing
+const h1 = document.querySelector("h1");
+
+// * Going downwards: children
+console.log(h1.querySelectorAll(".highlight"));
+console.log(h1.childNodes);
+console.log(h1.children);
+
+h1.firstElementChild.style.color = "red";
+h1.lastElementChild.style.color = "yellow";
+
+// * Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest(".header").style.background = "var(--gradient-secondary)";
+
+h1.closest("h1").style.background = "var(--gradient-primary)";
+
+// * Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(el => {
+  if (el !== h1) {
+    el.style.transform = "scale(0.5)";
+  }
+});
+
 // * 011. Event Delegation
 // * 009.Event Propagation: Bubbling and Capturing
 // * 010...in Practice
