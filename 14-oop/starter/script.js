d71138ae0797fc67370aa03ff93aae9c838d2230
resident7709 <resident7709@gmail.com>
2023-05-26 19:00:52 +0300

    ..setters-and-getters

--- 14-oop/starter/script.js
+++ 14-oop/starter/script.js
@@ -1,22 +1,57 @@
 "use strict";
 
-// * ES6 Classes
+// * 011.Setters and Getters
+
+// * 010.ES6 Classes
+
+// * 1.Classes aren't hoisted
+// * 2.Classes are first-class citizens
+// * 3.Classes are strict mode executed
+
+// const Person = class {}; // * Expression
+
+// * Declaration
+class Person {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName; // * instance
+    this.birthYear = birthYear; // * properties
+  }
+  // * ..added to prototype property
+  calcAge() {
+    console.log(2023 - this.birthYear);
+  }
+  greet() {
+    console.log(`Hi, ${this.firstName}!!`);
+  }
+}
+
+// Person.prototype.greet = function () {
+//   console.log(`Hi, ${this.firstName}!!`);
+// };
+
+const jessica = new Person("Jessica", 1982);
+
+console.log(jessica);
+console.log(jessica.__proto__ === Person.prototype);
+
+jessica.calcAge();
+jessica.greet();
 
 // * 005.Constructor functions and the new Operator
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName; // * instance
-  this.birthYear = birthYear; // * properties
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName; // * instance
+//   this.birthYear = birthYear; // * properties
 
-  // * unpractical way..
-  //   this.calcAge = function () {
-  //     console.log(2023 - birthYear);
-  //   };
-};
+// * unpractical way..
+//     this.calcAge = function () {
+//       console.log(2023 - birthYear);
+//     };
+// };
 
-const alex = new Person("Alex", 1971);
-const elena = new Person("Elena", 1971);
-const artur = new Person("Artur", 1992);
-const stas = new Person("Stas", 2003);
+// const alex = new Person("Alex", 1971);
+// const elena = new Person("Elena", 1971);
+// const artur = new Person("Artur", 1992);
+// const stas = new Person("Stas", 2003);
 
 // console.log(alex);
 // console.log(elena);
@@ -30,21 +65,21 @@ const stas = new Person("Stas", 2003);
 // * 4.Function automatically return {}
 
 // * 006.Prototypes
-console.log(Person.prototype);
+// console.log(Person.prototype);
 
-Person.prototype.calcAge = function () {
-  console.log(2023 - this.birthYear);
-};
+// Person.prototype.calcAge = function () {
+//   console.log(2023 - this.birthYear);
+// };
 
-alex.calcAge();
-stas.calcAge();
+// alex.calcAge();
+// stas.calcAge();
 
 // * prototype ~ prototypeOfLinkedObjects
 // console.log(alex.__proto__ === Person.prototype);
 // console.log(Person.prototype.isPrototypeOf(alex));
 // console.log(Person.prototype.isPrototypeOf(Person));
 
-Person.prototype.species = "homo spiritualis";
+// Person.prototype.species = "homo spiritualis";
 
 // console.log(alex.species);
 // console.log(alex, elena);
@@ -53,21 +88,21 @@ Person.prototype.species = "homo spiritualis";
 // Object.prototype (top of prototype chain)
 // console.log(alex.__proto__.__proto__);
 // console.log(alex.__proto__.__proto__.__proto__);
-console.dir(Person.prototype.constructor);
+// console.dir(Person.prototype.constructor);
 
-const arr = [9, 4, 6, 6, 4, 9, 8]; // * new Array === []
+// const arr = [9, 4, 6, 6, 4, 9, 8]; // * new Array === []
 
-console.log(arr.__proto__);
-console.log(arr.__proto__ === Array.prototype);
-console.log(arr.__proto__.__proto__);
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ === Array.prototype);
+// console.log(arr.__proto__.__proto__);
 
 // * unpractical way..
-Array.prototype.unique = function () {
-  return [...new Set(this)];
-};
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
 
-console.log(arr.unique());
+// console.log(arr.unique());
 
-const h1 = document.querySelector("h1");
+// const h1 = document.querySelector("h1");
 
-console.dir(x => x + 1);
+// console.dir(x => x + 1);
