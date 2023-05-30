acfae487cf23a2b1813cb71fb9303946f1d4f9d2
resident7709 <resident7709@gmail.com>
2023-05-30 10:07:13 +0300

    ..inheritance-between-'classes'-es6-classes

--- 14-oop/starter/challenges.js
+++ 14-oop/starter/challenges.js
@@ -1,20 +1,20 @@
 "use strict";
 
 // * CHALLENGE#1
-// const Car = function (make, speed) {
-//   this.make = make; // * instance
-//   this.speed = speed; // * properties
-// };
+const Car = function (make, speed) {
+  this.make = make; // * instance
+  this.speed = speed; // * properties
+};
 
-// Car.prototype.accelerate = function () {
-//   this.speed += 10;
-//   console.log(`${this.make} going at ${this.speed} km/h`);
-// };
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} going at ${this.speed} km/h`);
+};
 
-// Car.prototype.brake = function () {
-//   this.speed -= 5;
-//   console.log(`${this.make} going at ${this.speed} km/h`);
-// };
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} going at ${this.speed} km/h`);
+};
 
 // const bmw = new Car("BMW", 120);
 // const mercedes = new Car("Mercedes", 95);
@@ -53,16 +53,43 @@ class CarCl {
   }
 }
 
-const ford = new CarCl("Ford", 120);
+// const ford = new CarCl("Ford", 120);
 
-console.log(ford.speedUS);
+// console.log(ford.speedUS);
 
-ford.accelerate();
-ford.accelerate();
-ford.accelerate();
-ford.brake();
-ford.brake();
-ford.accelerate();
+// ford.accelerate();
+// ford.accelerate();
+// ford.accelerate();
+// ford.brake();
+// ford.brake();
+// ford.accelerate();
 
-ford.speedUS = 50;
-console.log(ford);
+// ford.speedUS = 50;
+// console.log(ford);
+
+// * CHALLENGE#3
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+// * Linking prototypes
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`
+  );
+};
+
+const tesla = new EV("Tesla", 120, 23);
+
+tesla.chargeBattery(90);
+tesla.brake();
+tesla.accelerate();
