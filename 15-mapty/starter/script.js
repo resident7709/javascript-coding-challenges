02703b55ba6df4af5357203dc78d85ba22e83a01
resident7709 <resident7709@gmail.com>
2023-06-02 09:15:54 +0300

    ..refactoring-for-project-architecture

--- 15-mapty/starter/script.js
+++ 15-mapty/starter/script.js
@@ -1,7 +1,19 @@
 "use strict";
 
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 const form = document.querySelector(".form");
 const containerWorkouts = document.querySelector(".workouts");
