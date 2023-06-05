cc39865c3ae2f541caf804f457dc673f40423a3e
resident7709 <resident7709@gmail.com>
2023-06-05 18:28:56 +0300

    ..promises-and-the-fetch-api

--- 16-asynchronous/starter/script.js
+++ 16-asynchronous/starter/script.js
@@ -3,7 +3,39 @@
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
-const getCountryData = function (country) {
+// * 007.Callback Hell
+setTimeout(() => {
+  console.log("1 second passed");
+  setTimeout(() => {
+    console.log("2 second passed");
+    setTimeout(() => {
+      console.log("3 second passed");
+    }, 1000);
+  }, 1000);
+}, 1000);
+
+const renderCountry = function (data, className = "") {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // * AJAX Call country 1
   const request = new XMLHttpRequest();
 
   request.open("GET", `https://restcountries.com/v2/name/${country}`);
@@ -11,29 +43,68 @@ const getCountryData = function (country) {
 
   request.addEventListener("load", function () {
     const [data] = JSON.parse(this.responseText);
-
     console.log(data);
+    // * render country 1
+    renderCountry(data);
 
-    const html = `
-      <article class="country">
-        <img class="country__img" src="${data.flag}" />
-        <div class="country__data">
-          <h3 class="country__name">${data.name}</h3>
-          <h4 class="country__region">${data.region}</h4>
-          <p class="country__row"><span>👫</span>${(
-            +data.population / 1000000
-          ).toFixed(1)}</p>
-          <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-          <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-        </div>
-      </article>
-      `;
+    // * get neighbour country
+    const neighbour = data.borders?.[0];
 
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-    countriesContainer.style.opacity = 1;
+    // const [neighbour] = data.borders;
+
+    // if(!neighbour) return;
+
+    // * AJAX Call country 2
+    const request2 = new XMLHttpRequest();
+
+    request2.open("GET", `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener("load", function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, "neighbour");
+    });
   });
 };
 
-getCountryData("portugal");
-getCountryData("russia");
-getCountryData("indonesia");
+// getCountryAndNeighbour("portugal");
+// getCountryAndNeighbour("russia");
+getCountryAndNeighbour("indonesia");
+
+// * 005.Our First AJAX Call: XMLHttpRequest
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+
+//   request.open("GET", `https://restcountries.com/v2/name/${country}`);
+//   request.send();
+
+//   request.addEventListener("load", function () {
+//     const [data] = JSON.parse(this.responseText);
+
+//     console.log(data);
+
+//     const html = `
+//       <article class="country">
+//         <img class="country__img" src="${data.flag}" />
+//         <div class="country__data">
+//           <h3 class="country__name">${data.name}</h3>
+//           <h4 class="country__region">${data.region}</h4>
+//           <p class="country__row"><span>👫</span>${(
+//             +data.population / 1000000
+//           ).toFixed(1)}</p>
+//           <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//           <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//         </div>
+//       </article>
+//       `;
+
+//     countriesContainer.insertAdjacentHTML("beforeend", html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+// getCountryData("portugal");
+// getCountryData("russia");
+// getCountryData("indonesia");
