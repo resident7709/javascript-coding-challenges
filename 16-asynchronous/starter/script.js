8fb056e4740867be8972efb2ff24954540801dcd
resident7709 <resident7709@gmail.com>
2023-06-06 16:32:18 +0300

    ..throwing-errors-manually

--- 16-asynchronous/starter/script.js
+++ 16-asynchronous/starter/script.js
@@ -3,6 +3,31 @@
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
+const renderCountry = function (data, className = "") {
+  const html = `
+      <article class="country ${className}">
+      <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)}</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+        </div>
+        </article>
+        `;
+
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  // countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText("beforeend", msg);
+  // countriesContainer.style.opacity = 1;
+};
+
 // * 008.Promises and the Fetch API
 // const request = new XMLHttpRequest();
 // request.open("GET", `https://restcountries.com/v2/name/${country}`);
@@ -12,28 +37,10 @@ const countriesContainer = document.querySelector(".countries");
 
 // console.log(request);
 
-const renderCountry = function (data, className = "") {
-  const html = `
-    <article class="country ${className}">
-    <img class="country__img" src="${data.flag}" />
-  <div class="country__data">
-  <h3 class="country__name">${data.name}</h3>
-  <h4 class="country__region">${data.region}</h4>
-  <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(
-    1
-  )}</p>
-      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-      </div>
-      </article>
-      `;
-
-  countriesContainer.insertAdjacentHTML("beforeend", html);
-  countriesContainer.style.opacity = 1;
-};
-
 // * 009.Consuming Promises
 // * 010.Chaining Promises
+// * 011.Handling Rejected Promises
+
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then(response => response.json())
@@ -44,10 +51,21 @@ const getCountryData = function (country) {
       return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
     })
     .then(response => response.json())
-    .then(data => renderCountry(data, "neighbour"));
+    .then(data => renderCountry(data, "neighbour"))
+    .catch(err => {
+      console.error(`${err} 💥💥💥`);
+      renderError(`Something went wrong..💥💥💥 ${err.message}. Try again!!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
 };
 
-getCountryData("cyprus");
+btn.addEventListener("click", function () {
+  getCountryData("portugal");
+});
+
+getCountryData("dhghddhdd");
 
 // const getCountryAndNeighbour = function (country) {
 // * AJAX Call country 1
