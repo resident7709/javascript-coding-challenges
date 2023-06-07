af90ea147ed063548a277a2762f2f70a55839f61
resident7709 <resident7709@gmail.com>
2023-06-07 08:51:11 +0300

    ..building-a-simple-promise

--- 16-asynchronous/starter/script.js
+++ 16-asynchronous/starter/script.js
@@ -28,6 +28,16 @@ const renderError = function (msg) {
   countriesContainer.style.opacity = 1;
 };
 
+// * 015.Event Loop in Practice
+console.log("Test start");
+setTimeout(() => console.log("0 sec timer"), 0);
+Promise.resolve("Resolved promise 1").then(res => console.log(res));
+Promise.resolve("Resolved promise 2").then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+});
+console.log("Test end");
+
 // * 008.Promises and the Fetch API
 // const request = new XMLHttpRequest();
 // request.open("GET", `https://restcountries.com/v2/name/${country}`);
