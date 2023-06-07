3588ef75274dd9092eda7c00247ef37aa23f67b1
resident7709 <resident7709@gmail.com>
2023-06-07 17:25:22 +0300

    ..consuming-promises-with-async-await

--- 16-asynchronous/starter/challenges.js
+++ 16-asynchronous/starter/challenges.js
@@ -1,29 +1,70 @@
 "use strict";
 
+// * CHALLENGE#2
+// const imgContainer = document.querySelector(".images");
+
+// const wait = secs => new Promise(resolve => setTimeout(resolve, secs * 1000));
+
+// const createImage = imgPath => {
+//   return new Promise((resolve, reject) => {
+//     const img = document.createElement("img");
+//     img.src = imgPath;
+
+//     img.addEventListener("load", () => {
+//       imgContainer.append(img);
+//       resolve(img);
+//     });
+
+//     img.addEventListener("error", () => reject(new Error("Image not found!!")));
+//   });
+// };
+
+// let currentImg;
+
+// createImage("./img/img-1.jpg")
+//   .then(img => {
+//     currentImg = img;
+//     console.log("Image 1 loaded");
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = "none";
+//     return createImage("./img/img-2.jpg");
+//   })
+//   .then(img => {
+//     currentImg = img;
+//     console.log("Image 2 loaded");
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = "none";
+//   })
+//   .catch(err => console.error(err));
+
 // * CHALLENGE#1
-const whereAmI = function (lat, lng) {
-  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-    .then(res => {
-      if (!res.ok) throw new Error(`Some problem with geocoding ${res.status}`);
-      return res.json();
-    })
-    .then(data => {
-      console.log(data);
-      console.log(`You are in ${data.city}, ${data.country}.`);
+// const whereAmI = function (lat, lng) {
+//   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+//     .then(res => {
+//       if (!res.ok) throw new Error(`Some problem with geocoding ${res.status}`);
+//       return res.json();
+//     })
+//     .then(data => {
+//       console.log(data);
+//       console.log(`You are in ${data.city}, ${data.country}.`);
 
-      return fetch(`https://restcountries.com/v2/name/${data.country}`);
-    })
-    .then(res => {
-      if (!res.ok) throw new Error(`Country not found ${res.status}`);
-      return res.json();
-    })
-    .then(data => renderCountry(data[0]))
-    .catch(err => {
-      console.error(`${err.message} 💥`);
-    });
-};
+//       return fetch(`https://restcountries.com/v2/name/${data.country}`);
+//     })
+//     .then(res => {
+//       if (!res.ok) throw new Error(`Country not found ${res.status}`);
+//       return res.json();
+//     })
+//     .then(data => renderCountry(data[0]))
+//     .catch(err => {
+//       console.error(`${err.message} 💥`);
+//     });
+// };
 
-whereAmI(52.508, 13.381);
+// whereAmI(52.508, 13.381);
 // whereAmI(19.037, 72.873);
 // whereAmI(-33.933, 18.474);
 
@@ -31,5 +72,3 @@ whereAmI(52.508, 13.381);
 // * Coordinates 1: 52.508, 13.381 (Latitude, Longitude)
 // * Coordinates 2: 19.037, 72.873
 // * Coordinates 3: -33.933, 18.474
-
-// * CHALLENGE#2
--- 16-asynchronous/starter/script.js
+++ 16-asynchronous/starter/script.js
@@ -65,41 +65,41 @@ const renderError = function (msg) {
 
 // * ====================================== * //
 
-const getPosition = () =>
-  new Promise((resolve, reject) =>
-    navigator.geolocation.getCurrentPosition(resolve, reject)
-  );
+// const getPosition = () =>
+//   new Promise((resolve, reject) =>
+//     navigator.geolocation.getCurrentPosition(resolve, reject)
+//   );
 
 // getPosition().then(pos => console.log(pos));
 
-const whereAmI = function () {
-  getPosition()
-    .then(pos => {
-      const { latitude: lat, longitude: lng } = pos.coords;
+// const whereAmI = function () {
+//   getPosition()
+//     .then(pos => {
+//       const { latitude: lat, longitude: lng } = pos.coords;
 
-      return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-    })
-    .then(res => {
-      if (!res.ok) throw new Error(`Some problem with geocoding ${res.status}`);
-      return res.json();
-    })
-    .then(data => {
-      console.log(data);
-      console.log(`You are in ${data.city}, ${data.country}.`);
+//       return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+//     })
+//     .then(res => {
+//       if (!res.ok) throw new Error(`Some problem with geocoding ${res.status}`);
+//       return res.json();
+//     })
+//     .then(data => {
+//       console.log(data);
+//       console.log(`You are in ${data.city}, ${data.country}.`);
 
-      return fetch(`https://restcountries.com/v2/name/${data.country}`);
-    })
-    .then(res => {
-      if (!res.ok) throw new Error(`Country not found ${res.status}`);
-      return res.json();
-    })
-    .then(data => renderCountry(data[0]))
-    .catch(err => {
-      console.error(`${err.message} 💥`);
-    });
-};
+//       return fetch(`https://restcountries.com/v2/name/${data.country}`);
+//     })
+//     .then(res => {
+//       if (!res.ok) throw new Error(`Country not found ${res.status}`);
+//       return res.json();
+//     })
+//     .then(data => renderCountry(data[0]))
+//     .catch(err => {
+//       console.error(`${err.message} 💥`);
+//     });
+// };
 
-btn.addEventListener("click", whereAmI);
+// btn.addEventListener("click", whereAmI);
 
 // * 015.Event Loop in Practice
 // console.log("Test start");
