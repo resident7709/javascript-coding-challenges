6f1fbd57152010b61df127bb3fcece2586fa76c2
resident7709 <resident7709@gmail.com>
2023-06-23 11:06:45 +0300

    ..implementing-pagination-part-1

--- 18-forkify-app/src/js/controller.js
+++ 18-forkify-app/src/js/controller.js
@@ -1,9 +1,14 @@
 import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
 import searchView from "./views/searchView.js";
+import resultsView from "./views/resultsView.js";
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -25,11 +30,14 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     const query = searchView.getQuery(); // * get search query
     if (!query) return;
 
     await model.loadSearchResults(query); // * load search results
-    console.log(model.state.search.results); // * render results
+
+    resultsView.render(model.state.search.results); // * render results
   } catch (err) {
     console.error(err);
   }
